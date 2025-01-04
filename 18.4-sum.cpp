3095038956da89ddaffee22118955fa4281cae98
Achyran <tobias.breitkopf@googlemail.com>
2025-01-04 22:23:37 +0100

    Fixed Intelesense and added header file for needed libs

---
+++ 18.4-sum.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=18 lang=cpp
+ *
+ * [18] 4Sum
+ */
+
+// @lc code=start
+#include "headers.h"
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
