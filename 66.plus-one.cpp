cb0a53451b68e0fc90207e284055bf7b90bcb22e
Achyran <tobias.breitkopf@googlemail.com>
2025-01-05 13:04:41 +0100

    Use different aproache

--- 66.plus-one.cpp
+++ 66.plus-one.cpp
@@ -9,29 +9,8 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int amount = 0;
-        for (int i = 0; i < digits.size(); i++)
-        {
-            amount += digits[i] * pow(10 ,(digits.size()-i)-1);
-        }
-
-        amount ++;
-
-        vector<int> output;
-        
-
-        while (amount >0)
-        {
-            int mod = amount %10;
-            output.emplace(output.begin(),mod);
-            amount = amount/10;
-        }
-        
-
-        return output;
         
         
-
     }
 };
 // @lc code=end
