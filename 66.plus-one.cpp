ccad7f792eefa12a39386e2a678ecc143f56e659
Achyran <tobias.breitkopf@googlemail.com>
2025-01-05 12:46:12 +0100

    Fixed Array to int

--- 66.plus-one.cpp
+++ 66.plus-one.cpp
@@ -5,14 +5,14 @@
  */
 
 // @lc code=start
-//#include "headers.h"
+#include "headers.h"
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int amount = 0;
         for (int i = 0; i < digits.size(); i++)
         {
-            amount += digits[i] * (10 * (digits.size()-i));
+            amount += digits[i] * pow(10 ,(digits.size()-i)-1);
         }
 
         return {amount};
