4dafe72ccfa616284593f3186fe0ea75c5a09e87
Achyran <tobias.breitkopf@googlemail.com>
2025-01-06 19:59:34 +0100

    First atempt

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -5,10 +5,35 @@
  */
 
 // @lc code=start
+#include "headers.h"
+
 class Solution {
 public:
     bool isPalindrome(string s) {
+        stack<char> sta;
+
+        bool isOdd = s.size() %2;
+
+        bool stackup = true;
+
+        for (int i = 0; i < s.size()/2; i++)
+        {
+            if(isalnum(s[i])){
+                sta.push( tolower(s[i]));
+            }
+        }
+
+        int start = (s.size() % 2) ? s.size()/2+1:s.size();
+
+        for (int i = start +1; i < s.size(); i++)
+        {
+            if(sta.top() != tolower(s[i])) return false;
+            sta.pop();
+        }
         
+        
+        
+        return true;
     }
 };
 // @lc code=end
