16b264a882f979eadf316cb86381fac86764bba7
Achyran <tobias.breitkopf@googlemail.com>
2025-01-06 19:31:19 +0100

    Exepted

--- 69.sqrt-x.cpp
+++ 69.sqrt-x.cpp
@@ -5,10 +5,33 @@
  */
 
 // @lc code=start
+//#include "headers.h"
 class Solution {
 public:
     int mySqrt(int x) {
         
+        if(x == 0) return 0;
+
+        unsigned int lowerBound = 0;
+        unsigned int upperBound = x;
+        unsigned int output = 1;
+
+        while (lowerBound <= upperBound)
+        {
+            unsigned int cur = lowerBound + ((upperBound-lowerBound)/2);
+            
+            cout << cur << endl;
+
+            if(cur > x/cur){
+                upperBound = cur-1;
+            }else if ( cur < x/cur){
+                lowerBound = cur +1;
+                output = cur;
+            }else return cur;
+        }
+        
+
+        return output;
     }
 };
 // @lc code=end
