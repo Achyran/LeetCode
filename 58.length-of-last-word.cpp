55f89a74ab117fb73e4d77e1be44160169d89499
Achyran <tobias.breitkopf@googlemail.com>
2025-01-06 20:31:34 +0100

    First try

--- 58.length-of-last-word.cpp
+++ 58.length-of-last-word.cpp
@@ -5,10 +5,28 @@
  */
 
 // @lc code=start
+//#include "headers.h"
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        int count = 0;
+        bool wordStarted = false;
+
+        for (int i = s.size(); i >= 0; i--)
+        {
+            if(wordStarted) count ++;
+
+            if(!wordStarted && isalpha(s[i])) wordStarted = true;
+
+            if(wordStarted && !isalpha(s[i])) break;
+        }
         
+
+
+
+        return count;
+
+
     }
 };
 // @lc code=end
