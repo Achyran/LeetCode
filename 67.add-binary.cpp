6da3b61cf7db7ba2167eeaef8cde5710c4124acc
Achyran <tobias.breitkopf@googlemail.com>
2025-01-07 20:51:09 +0100

    Ready for testing

--- 67.add-binary.cpp
+++ 67.add-binary.cpp
@@ -23,9 +23,36 @@ public:
             add = a;
         }
 
-        
+        for (int i = base.length()-1; i <= base.length()-add.length(); i--)
+        {
+            int temp = carry;
+            if(base[i] == '1') temp ++;
+            if(add[i] == '1') temp ++;
 
+            switch (temp)
+            {
+            case 0:
+                base[i] = '0';
+                break;
+            case 1:
+                base[i] = '1';
+                break;
+            case 2:
+                carry = 1;
+                base[i] = '1';
+            case 3:
+                carry =1;
+                base[i] = '1';
+                break;
+            }
 
+        }
+
+        if(base.length() == add.length && carry == 1){
+            base = '1' + base;
+        }
+
+        return base;
     }
 };
 // @lc code=end
