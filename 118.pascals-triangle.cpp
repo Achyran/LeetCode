ec6f3c84286ecb5d9c6036afd2628a972185e54d
Achyran <tobias.breitkopf@googlemail.com>
2025-01-08 20:04:05 +0100

    ready for testing

--- 118.pascals-triangle.cpp
+++ 118.pascals-triangle.cpp
@@ -5,10 +5,28 @@
  */
 
 // @lc code=start
+#include "headers.h"
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         
+        if( numRows == 1) return {{1}};
+        if( numRows == 2) return {{1}{1,1}};
+
+        vector<vector<int>> output = {{1}{1,1}};
+
+        for (int i = 2; i < numRows; i++)
+        {
+            output.push_back({1});
+            for (int j = 1; j < output[i-1].size(); j++)
+            {
+                output[i].push_back(output[i-1][j] + output[i-1][j-1]);
+            }
+            output[i].push_back(1);
+            
+        }
+        
+
     }
 };
 // @lc code=end
