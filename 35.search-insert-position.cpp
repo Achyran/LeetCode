42055014baf503c6ea6fb9b904a6e58339d30025
Achyran <tobias.breitkopf@googlemail.com>
2025-01-08 19:13:49 +0100

    Ready for testing

--- 35.search-insert-position.cpp
+++ 35.search-insert-position.cpp
@@ -11,7 +11,13 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == target) return i;
+            if(nums[i] > target) return i -1;
+        }
         
+        return nums.size() +1;
     }
 };
 // @lc code=end
