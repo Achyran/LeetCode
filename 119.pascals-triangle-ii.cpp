0376c03495c92fea4ded2781b0866a79d5d607f2
Achyran <tobias.breitkopf@googlemail.com>
2025-01-08 20:10:36 +0100

    Fixed buggs

--- 119.pascals-triangle-ii.cpp
+++ 119.pascals-triangle-ii.cpp
@@ -10,12 +10,12 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         
-        if( numRows == 1) return {1};
-        if( numRows == 2) return {1,1};
+        if( rowIndex == 1) return {1};
+        if( rowIndex == 2) return {1,1};
 
         vector<vector<int>> output = {{1},{1,1}};
 
-        for (int i = 2; i < numRows; i++)
+        for (int i = 2; i < rowIndex+1; i++)
         {
             output.push_back({1});
             for (int j = 1; j < output[i-1].size(); j++)
