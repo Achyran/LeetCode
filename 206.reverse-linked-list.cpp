cac4e1d23149a2226414a303b7d13c78d966e767
Achyran <tobias.breitkopf@googlemail.com>
2025-01-09 20:02:34 +0100

    ready for test

--- 206.reverse-linked-list.cpp
+++ 206.reverse-linked-list.cpp
@@ -5,20 +5,34 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+//  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+#include "headers.h"
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* next = head->next;
+        ListNode* prev = nullptr;
+
+        do {
+            current->next = prev;
+            prev = current;
+            current = next;
+            next = current->next;
+            
+        }
+        while (next != nullptr);
         
+        return current;
     }
 };
 // @lc code=end
