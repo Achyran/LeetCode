1812e831a0dc91eb25c486263b5477b586ec44d5
Achyran <tobias.breitkopf@googlemail.com>
2025-01-09 20:10:17 +0100

    ready for testing

--- 217.contains-duplicate.cpp
+++ 217.contains-duplicate.cpp
@@ -9,7 +9,17 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
+        unordered_map<int,int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(map.find(nums[i]) == map.end()){
+                map[nums[i]] = i;
+            }else return true;
+        }
         
+        return false;
     }
 };
 // @lc code=end
