f131701ff8d99f3994e490b648af4744e712a113
Achyran <tobias.breitkopf@googlemail.com>
2025-01-09 20:13:44 +0100

    started

--- 219.contains-duplicate-ii.cpp
+++ 219.contains-duplicate-ii.cpp
@@ -5,10 +5,18 @@
  */
 
 // @lc code=start
+#include "headers.h"
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            /* code */
+        }
         
+
     }
 };
 // @lc code=end
