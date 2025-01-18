94860da08ecacf04eacb77f29ac220482532216c
Achyran <tobias.breitkopf@googlemail.com>
2025-01-18 21:49:46 +0100

    f

--- 83.remove-duplicates-from-sorted-list.cpp
+++ 83.remove-duplicates-from-sorted-list.cpp
@@ -5,20 +5,22 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/**/ 
+  //Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ /**/
+ #include "headers.h"
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         
+        return head;
     }
 };
 // @lc code=end
