f44186bbcf326b7905ebda386dd73dbcb528443a
Achyran <tobias.breitkopf@googlemail.com>
2025-01-19 17:46:50 +0100

    break Condition

--- 104.maximum-depth-of-binary-tree.cpp
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -22,6 +22,14 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         
+        if(root->left == nullptr && root->right == nullptr) return 0;
+
+        int cLeft = 0;
+        int cRight = 0;
+
+        return 0;
+
+
     }
 };
 // @lc code=end
