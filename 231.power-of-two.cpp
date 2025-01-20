0da394db2afa6ddcfc3e22393934093c7b529dff
Achyran <tobias.breitkopf@googlemail.com>
2025-01-20 21:42:35 +0100

    ready for testing

--- 231.power-of-two.cpp
+++ 231.power-of-two.cpp
@@ -8,12 +8,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        
+        int res = 0;
         for (int i = 0; i < 32; i++)
         {
 
-            
+            if(n & 1) res++;
+            if(res == 2) return false;
+            n >>= 1;
             /* code */
         }
+        return true;
         
         
     }
