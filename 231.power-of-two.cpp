5962817f63e043b81d9d678ba4eae84452402ad4
Achyran <tobias.breitkopf@googlemail.com>
2025-01-20 21:38:19 +0100

    started 231

---
+++ 231.power-of-two.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=231 lang=cpp
+ *
+ * [231] Power of Two
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for (int i = 0; i < 32; i++)
+        {
+
+            
+            /* code */
+        }
+        
+        
+    }
+};
+// @lc code=end
+
