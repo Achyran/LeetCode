932a9e6c8b90950d1b607376c6d00d1daa1396ee
Achyran <tobias.breitkopf@googlemail.com>
2025-01-20 21:26:41 +0100

    Include headers

--- 191.number-of-1-bits.cpp
+++ 191.number-of-1-bits.cpp
@@ -5,9 +5,19 @@
  */
 
 // @lc code=start
+#include "headers.h"
 class Solution {
 public:
     int hammingWeight(int n) {
+        int res = 0;
+        
+        for (int i = 0; i < 32; i++)
+        {
+            res <<= 1;
+            res |= n & 1;
+            n >>= 1;
+            n >>= 1;
+        }
         
     }
 };
