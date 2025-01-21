baf34ed4d207c3f9c72d654529adc2e3ce6201da
Achyran <tobias.breitkopf@googlemail.com>
2025-01-21 21:11:01 +0100

    fixed loop dir

--- 55.jump-game.cpp
+++ 55.jump-game.cpp
@@ -11,7 +11,7 @@ public:
     bool canJump(vector<int>& nums) {
         int gool = nums.size()-1;
 
-        for (int i = nums.size() -1; i >= 0; i++)
+        for (int i = nums.size() -1; i >= 0; i--)
         {
             if(i + nums[i] >= gool){
                 gool = i;
