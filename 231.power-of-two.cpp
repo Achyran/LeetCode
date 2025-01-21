094e4bede3405670b4f1b076e6f7eea557255726
Achyran <tobias.breitkopf@googlemail.com>
2025-01-21 21:05:24 +0100

    optimized

--- 231.power-of-two.cpp
+++ 231.power-of-two.cpp
@@ -14,14 +14,11 @@ public:
         int res = 0;
         for (int i = 0; i < 32; i++)
         {
-
             if(n & 1) res++;
             if(res == 2) return false;
             n >>= 1;
-            /* code */
         }
         return true;
-        
         
     }
 };
