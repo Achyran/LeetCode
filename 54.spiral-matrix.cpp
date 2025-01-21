fa26d04666080fe06d0880850041d20e861ade1c
Achyran <tobias.breitkopf@googlemail.com>
2025-01-21 21:30:49 +0100

    c

--- 54.spiral-matrix.cpp
+++ 54.spiral-matrix.cpp
@@ -10,13 +10,37 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int left = 0;
-        int right = matrix.size()-1;
+        int right = matrix[0].size()-1;
         int top = 0;
-        int bottom = matrix[0].size()-1;
+        int bottom = matrix.size()-1;
+        vector<int> result;
 
-        while ()
+        while (left < right && top < bottom)
         {
-            /* code */
+            for (int i = left ; i < right ; i++)
+            {
+                result.push_back(matrix[top][i]);
+            }
+            top ++;
+            for (int i = top; i < bottom; i++)
+            {
+                result.push_back(matrix[i][right]);
+            }
+            right --;
+
+            for(int i = right; i >= left; i --){
+                result.push_back(matrix[bottom][i]);
+            }
+            bottom ++;
+
+            for(int i = bottom; i >= top; i --){
+                result.push_back(matrix[i][left]);
+            }
+            
+            
+            
+            
+            
         }
         
 
