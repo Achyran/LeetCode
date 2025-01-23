0aa59d1525d69ca8ac1caed0c8c41797e9047ced
Achyran <tobias.breitkopf@googlemail.com>
2025-01-23 21:26:56 +0100

    Done

--- 258.add-digits.cpp
+++ 258.add-digits.cpp
@@ -10,17 +10,13 @@ public:
     int addDigits(int num) {
         
         if(num < 10) return num;
-
         int amount = 0;
         while (num > 0)
         {
             amount += num % 10;
             num = num/10;
         }
-
-        addDigits(amount);
-        
-        
+        return addDigits(amount);
     }
 };
 // @lc code=end
