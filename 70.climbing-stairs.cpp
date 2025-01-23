791760fef02d94099355f07b756ab93313f41c06
Achyran <tobias.breitkopf@googlemail.com>
2025-01-23 21:11:40 +0100

    fixed

--- 70.climbing-stairs.cpp
+++ 70.climbing-stairs.cpp
@@ -13,13 +13,13 @@ public:
         if(n == 1) return 1;
         if(n == 2) return 2;
 
-        int steps[n];
+        int steps[n] ;
         steps[n-1] = 1;
         steps[n-2] = 1;
 
         for (int i = n-3; i >= 0; i--)
         {
-            steps[i] = step[i +1] + steps[i+2];
+            steps[i] = steps[i +1] + steps[i+2];
         }
         
 
