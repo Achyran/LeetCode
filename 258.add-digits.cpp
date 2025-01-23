ddcda4db71e070c141aff34680b8aaaefc66acfa
Achyran <tobias.breitkopf@googlemail.com>
2025-01-23 21:25:39 +0100

    ready for test

---
+++ 258.add-digits.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=258 lang=cpp
+ *
+ * [258] Add Digits
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num < 10) return num;
+
+        int amount = 0;
+        while (num > 0)
+        {
+            amount += num % 10;
+            num = num/10;
+        }
+
+        addDigits(amount);
+        
+        
+    }
+};
+// @lc code=end
+
