71f91627f360a21a501ce7e367ce7610ae177b4d
Achyran <tobias.breitkopf@googlemail.com>
2025-01-26 15:07:57 +0100

    Loop

--- 169.majority-element.cpp
+++ 169.majority-element.cpp
@@ -10,6 +10,10 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int,int> map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            /* code */
+        }
         
     }
 };
