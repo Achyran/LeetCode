8f2f8939083221272da90596ffe2164ed2c69f61
Achyran <tobias.breitkopf@googlemail.com>
2025-01-26 15:09:29 +0100

    Ready for test

--- 169.majority-element.cpp
+++ 169.majority-element.cpp
@@ -12,9 +12,11 @@ public:
         unordered_map<int,int> map;
         for (int i = 0; i < nums.size(); i++)
         {
-            /* code */
+            map[nums[i]] ++;
+            if(map[nums[i]] > nums.size()/2) return nums[i];
         }
         
+        return 0;
     }
 };
 // @lc code=end
