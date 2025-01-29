0f1367a778396567f8133ff47e9b9b078647581f
Achyran <tobias.breitkopf@googlemail.com>
2025-01-29 22:04:53 +0100

    added loop

--- 202.happy-number.cpp
+++ 202.happy-number.cpp
@@ -10,13 +10,26 @@
 class Solution {
 public:
     bool isHappy(int n) {
+        unordered_map<int,int> map;
+
+        return isHappyHelper(n,map)
         
     }
 
 private:
-    bool isHappyHelper(int n, unordered_map<int,int> map){
+    bool isHappyHelper(int n, unordered_map<int,int>* map){
         if(n == 1) return true;
-        if(map.find(n) != map.end()) return false;
+        if(map->find(n) != map->end()) return false;
+        int m = 0;
+        while (n > 0)
+        {
+            int a = n%10;
+            m += a * a;
+            n = n/10;
+        }
+
+        
+        
         
     }
 
