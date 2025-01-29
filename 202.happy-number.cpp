41227560a78753b4857fa0412018e202b7a5cfba
Achyran <tobias.breitkopf@googlemail.com>
2025-01-29 21:54:28 +0100

    start 202

---
+++ 202.happy-number.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=202 lang=cpp
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+    }
+
+
+};
+// @lc code=end
+
