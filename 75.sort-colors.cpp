41691313577fd00c693ede2de00769c6670ccb8f
Achyran <tobias.breitkopf@googlemail.com>
2025-01-31 18:39:14 +0100

    Finished 75

--- 75.sort-colors.cpp
+++ 75.sort-colors.cpp
@@ -5,10 +5,38 @@
  */
 
 // @lc code=start
+//#include "headers.h"
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        if(nums.size() == 1) return;
+
+        unordered_map<int,int> map;
+
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(map.find(nums[i]) != map.end()){
+                map[nums[i]]++;
+            }else{
+                map[nums[i]] = 1;
+            }
+        }
+        int counter = 0;
+        int currentCol = 0;
+        while (counter < nums.size()){
+            if(map[currentCol] == 0){
+                currentCol ++;
+            }else{
+
+                nums[counter] = currentCol;
+                counter ++;
+                map[currentCol] --;
+            }
+        }
+
         
+
     }
 };
 // @lc code=end
