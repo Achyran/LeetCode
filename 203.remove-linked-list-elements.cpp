11fb1c5e1ce867e71da9ab923f15c455640a4c59
Achyran <tobias.breitkopf@googlemail.com>
2025-02-01 22:39:16 +0100

    enabled list

--- 203.remove-linked-list-elements.cpp
+++ 203.remove-linked-list-elements.cpp
@@ -6,15 +6,16 @@
 
 // @lc code=start
 #include "headers.h"
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+/* */
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ /**/
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
