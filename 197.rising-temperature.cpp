8493ebeef3aaae8b6426b68cf05a234e5013d519
Achyran <tobias.breitkopf@googlemail.com>
2025-02-01 22:36:38 +0100

    Select only id

--- 197.rising-temperature.cpp
+++ 197.rising-temperature.cpp
@@ -1,4 +1,4 @@
-SELECT * FROM Weather as Today
+SELECT Today.id FROM Weather as Today
 JOIN Weather as Yesterday
 ON (DATE_SUB(Today.recordDate, INTERVAL 1 DAY) = Yesterday.recordDate)
 WHERE Today.temperature > Yesterday.temperature;
