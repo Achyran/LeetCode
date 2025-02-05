fc2526745cc8fff80588d39b5ed4be12feea1b83
Achyran <tobias.breitkopf@googlemail.com>
2025-02-05 19:27:53 +0100

    second helper

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -16,6 +16,18 @@ public:
 private:
     vector<vector<int>> helper(int x, vector<int>& nums){
         if(nums.size() == 1) return { {x,nums[0]} };
+
+        for(int i = 0; i < nums.size(); i++){
+            helper(nums[i], )
+        }
+    }
+
+    vector<int> removeX (int x ,vector<int>& nums){
+        vector<int> output;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[x] != x) output.push_back(nums[i]);
+        }
+        return output;
     }
     
 };
