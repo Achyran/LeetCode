56820cc65c61fce23676a22cbd3da0cf448ab64d
Achyran <tobias.breitkopf@googlemail.com>
2025-02-05 20:28:33 +0100

    done

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -5,53 +5,30 @@
  */
 
 // @lc code=start
-#include "headers.h"
+//#include "headers.h"
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        if(nums.size() == 1) return { nums };
+        if(nums.size() <= 1) return { nums };
 
         vector<vector<int>> output;
         for(int i =0; i < nums.size(); i++){
-            vector<int> redusced = removeX(nums[i],nums);
-            vector<vector<int>> permute = helper(nums[i],nums);
-            Combine(output,permute);
+            vector<int> reduced(nums.begin(), nums.end());
+            reduced.erase(reduced.begin() + i);
+            vector<vector<int>> res = permute(reduced);
+            
+            for(int j = 0; j < res.size();j++){
+                vector<int> v = res[j];
+                v.insert(v.begin(),nums[i]);
+                output.push_back(v);
+            }
         }
 
         return output;
 
         
     }
-private:
-    vector<vector<int>> helper(int x, vector<int>& nums){
-        if(nums.size() == 1) return { {x,nums[0]} };
-
-        vector<vector<int>> output;
-        for(int i = 0; i < nums.size(); i++){
-            vector<int> redusced = removeX(nums[i],nums);
-            vector<vector<int>> permute = helper(nums[i],nums);
-            Combine(output, permute);
-        }
-        return output;
-    }
-
-    vector<int> removeX (int x ,vector<int>& nums){
-        vector<int> output;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[x] != x) output.push_back(nums[i]);
-        }
-        return output;
-    }
-
-    vector<vector<int>>& Combine(vector<vector<int>>& a, vector<vector<int>>& b)
-    {
-        for(int j = 0; j < b.size(); j ++)
-        {
-            a.push_back(b[j]);
-        }
-
-        return a;
-    }
+ 
     
 };
 // @lc code=end
