435818467f205634bc4eb32af16334be5b5d563e
Achyran <tobias.breitkopf@googlemail.com>
2025-02-09 19:46:08 +0100

    removed linkt list

--- 71.simplify-path.cpp
+++ 71.simplify-path.cpp
@@ -7,52 +7,10 @@
 // @lc code=start
 #include "headers.h"
 
-struct LinkedNode
-{
-    public:
-        int data;
-        LinkedNode* next;
-        LinkedNode* prev;
-};
-
-class ManagedList
-{
-public:
-    LinkedNode* start;
-    LinkedNode* head;
-    ManagedList(LinkedNode* pStart);
-    ~ManagedList();
-    void Push();
-    void Pop();
-
-};
-
-
-
-
-ManagedList::ManagedList(LinkedNode* pStart)
-{
-    start = pStart;
-    head = start;
-}
-
-void ManagedList::Push(){
-    
-}
-
-ManagedList::~ManagedList()
-{
-}
-
-
-
-
 class Solution {
 public:
     string simplifyPath(string path) {
         
-        LinkedNode* start = nullptr;
-        LinkedNode* head = nullptr;
 
         string tempFileName = "";
         
