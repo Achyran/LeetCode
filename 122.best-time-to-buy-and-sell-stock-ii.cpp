65708734600a5f3ac927d1c1c23f3fa7a601a272
Achyran <tobias.breitkopf@googlemail.com>
2025-02-10 19:27:17 +0100

    Loop

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -9,6 +9,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            /* code */
+        }
         
     }
 };
