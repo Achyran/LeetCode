95def0e8c2996c9865d700c86008b450d20b3a35
Achyran <tobias.breitkopf@googlemail.com>
2025-02-13 22:23:43 +0100

    Start fresh

--- 50.pow-x-n.cpp
+++ 50.pow-x-n.cpp
@@ -8,24 +8,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-
-        
-     
-        return Pow(x,n);
-    }
-private:
-    double Pow(double x, int n){
         if (x == 0) return 0;
         if (n == 0) return 1;
-
-        double res = Pow(x,n/2);
-
-        res = res * res;
-        if(n/2 == 0){
-            return res;
-        }else{
-            return x * res;
-        }
+        
+     
+        
     }
 };
 // @lc code=end
