a89eebad19e183f89fb6b5ee859216a3ae93c609
Achyran <tobias.breitkopf@googlemail.com>
2025-02-13 22:37:43 +0100

    c

--- 50.pow-x-n.cpp
+++ 50.pow-x-n.cpp
@@ -9,15 +9,28 @@ class Solution {
 public:
     double myPow(double x, int n) {
         
-        bool 
-        
+        bool negativ = isNegativ(n);
+        n = abs(n);
         if (x == 0) return 0;
         if (n == 0) return 1;
+
+        return negativ? 1/ powRec(x,n): powRec(x,n);
         
      
         
     }
 private:
+
+    double powRec(double x,int n){
+        
+        if(n == 0) return 1;
+        if (x == 0) return 0;
+
+        double res = powRec(x,n/2);
+        res = res * res;
+        return n%2 == 1? x * res: res;
+    }
+
     bool isNegativ(int numb){
         numb = numb >> 31;
         return numb;
