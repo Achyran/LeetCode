7ed5503f1d91e1995ef211f3165154bb39ad687b
Achyran <tobias.breitkopf@googlemail.com>
2025-02-13 22:20:17 +0100

    To slow

--- 29.divide-two-integers.cpp
+++ 29.divide-two-integers.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-#include "headers.h"
+//#include "headers.h"
 class Solution {
 public:
     int divide(int dividend, int divisor) {
@@ -19,11 +19,11 @@ public:
         //if(dividend == INT_MIN) dividend = -2147483647;
         //if(divisor == INT_MIN) divisor = -2147483647;
 
-        long l_dividend = abs(dividend);
-        long l_divisor = abs(divisor);
+        long l_dividend = labs(dividend);
+        long l_divisor = labs(divisor);
         
 
-        int counter = 0;
+        long counter = 0;
         while(l_dividend>= l_divisor ){
             l_dividend -= l_divisor;
             counter ++;
